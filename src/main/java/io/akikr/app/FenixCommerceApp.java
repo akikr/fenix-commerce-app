0c5c0f782aaf58eec69019a29733fc173240bc3f
ankit <akikr@duck.com>
2026-01-25 17:44:55 +0530

    Add: The java docs for main class

--- src/main/java/io/akikr/app/FenixCommerceApp.java
+++ src/main/java/io/akikr/app/FenixCommerceApp.java
@@ -8,12 +8,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+/**
+ * Main entry point for the Fenix Commerce application. This class initializes the Spring Boot
+ * application context.
+ */
 @SpringBootApplication
 @EnableConfigurationProperties(value = {AppLoggingProperties.class})
 public class FenixCommerceApp {
 
   private static final Logger log = LoggerFactory.getLogger(FenixCommerceApp.class);
 
+  /**
+   * Main method to start the application.
+   *
+   * @param args command line arguments
+   */
   public static void main(String[] args) {
     log.info("Application staring with args:[{}]", Arrays.toString(args));
     SpringApplication.run(FenixCommerceApp.class, args);
